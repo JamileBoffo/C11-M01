57a1ba8f6de45b319ad16afe9689c7a022fd4f17
JamileBoffo <jamileboffo@gmail.com>
2022-02-01 15:48:13 -0300

    master

---
+++ Aula10/Ex04.js
@@ -0,0 +1,14 @@
+const prompt = require('prompt-sync')();
+console.clear();
+
+let list = []
+let fil1 = ['0','0','0']
+let fil2 = ['1','1','1']
+let fil3 = ['2','2','2']
+list.push(fil1);
+list.push(fil2);
+list.push(fil3);
+console.log(list);
+console.log(fil1);
+console.log(fil2);
+console.log(fil3);
