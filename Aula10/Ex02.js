7cd82134fab07f7d5fa0285179c2b318907164cf
JamileBoffo <jamileboffo@gmail.com>
2022-02-01 11:48:39 -0300

    master

--- Aula10/Ex02.js
+++ Aula10/Ex02.js
@@ -2,20 +2,29 @@ const prompt = require('prompt-sync')();
 console.clear();
 
 let quant = +prompt('Digite a quantidade de nomes a serem adicionadas: ');
-let tam = []
+let maior = ''
 let lista = [];
 
 for (let i = 0; i < quant; i++){
     var nome = prompt('Digite um nome: ');
     lista.push(nome);
-    var nm = lista[i]
-    tam.push(nm.length)
-    var maxi = (Math.max(...tam));
-    var mini = (Math.min(...tam));
 }
 console.log(lista)
 let rev = lista.reverse();
 console.log(rev)
-console.log(`O maior nome é ${maxi} e o menor nome é ${mini}`)
+
+for (let i of lista){
+    if (i.length > maior.length){
+        maior = i
+    }
+    
+}
+let menor = maior
+for(let i of lista){
+    if(i.length < menor.length){
+        menor = i
+    }
+}
+console.log(`O maior nome é ${maior} e o menor nome é ${menor}`)
 
 
