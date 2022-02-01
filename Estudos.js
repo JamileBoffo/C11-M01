02bcba1d9851cace05c86c63f52084b72aa22756
JamileBoffo <jamileboffo@gmail.com>
2022-02-01 11:50:43 -0300

    master

---
+++ Estudos.js
@@ -0,0 +1,42 @@
+console.clear()
+const prompt = require('prompt-sync')(); // REQUISIÇÃO PARA RESPOSTA DE USUARIO
+
+/*let num1 = +prompt('Digite uum numero: ');
+let num2 = +prompt('Digite outro numero: ');
+console.log(`Adição: ${num1} + ${num2} = ${num1 + num2}`);
+console.log(`Subtração: ${num1} - ${num2} = ${num1 - num2}`);
+console.log(`Multiplicação: ${num1} * ${num2} = ${num1 * num2}`);
+console.log(`Divisão: ${num1} / ${num2} = ${num1 / num2}`);
+console.log(`Resto da divisão: ${num1} / ${num2} = ${num1 % num2}`);
+console.log(`Exponenciação: ${num1} elevado a ${num2} = ${num1 ** num2}`);
+*/
+
+// TRANSFORMANDO NUMEROS EM STRING
+/*const num = 123;
+console.log(num);
+console.log(typeof (num));
+console.log()
+const numString = num.toString();
+console.log(numString);
+console.log(typeof(numString));*/
+
+// TRANSFORMANDO STRING EM NUMEROS
+/*const num = '123';
+console.log(num);
+console.log(typeof (num));
+console.log()
+const toNumber = Number(num);
+console.log(toNumber);
+console.log(typeof(toNumber));*/
+
+// TRANFORMAR TODAS AS STRINGS EM LETRA MINUSCULA
+/*const nome = prompt('Digite seu nome: ');
+const nomeLower = nome.toLowerCase()
+console.log(nome);
+console.log(nomeLower);*/
+
+// TRANSFORMAR TODAS AS STRINGS EM LETRA MAIUSCULA
+/*nst nome = prompt('Digite seu nome: ');
+const nomeUpper = nome.toUpperCase()
+console.log(nome);
+console.log(nomeUpper);*/
