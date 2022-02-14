2ffed2cbcc629464a582e844af8485f4aa6da6d8
JamileBoffo <jamileboffo@gmail.com>
2022-02-14 16:10:55 -0300

    master

--- Boolean&Condicionais/Maior_Menor_Igual.js
+++ Boolean&Condicionais/Maior_Menor_Igual.js
@@ -6,7 +6,7 @@ let num2 = +prompt('Digite outro numero: ');
 
 if (num1 > num2){
     console.log(`${num1} é maior que ${num2}`);
-} else if (num1 < num 2) {
+} else if (num1 < num2) {
     console.log(`${num2} é maior que ${num1}`);
 } else {
     console.log(`${num1} e ${num2} são iguais`);
